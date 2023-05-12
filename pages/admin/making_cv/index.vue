09a4ae80601349d8471c505021cfcfb179079f96
do minh <minh.dc.2064@aptechlearning.edu.vn>
2023-05-13 05:12:30 +0700

    hackathon

---
+++ pages/admin/making_cv/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <client-only>
+    <h1>Making CV</h1>
+  </client-only>
+</template>
+
+<script>
+import { mapFields } from "vuex-map-fields";
+import { mapActions } from "vuex";
+
+
+export default {
+  layout: "adminDev",
+  components: {
+
+  },
+  
+  name: "MakingCv",
+  data() {
+    return {};
+  },
+  async mounted() {
+  },
+  computed: {
+    ...mapFields("admin/game/ninjas", [
+      "ninjas",
+
+    ]),
+  },
+  methods: {
+    ...mapActions("admin/game/ninjas", ["fetchAccountNinjas"]),
+  },
+};
+</script>
+<style >
+
+</style>
