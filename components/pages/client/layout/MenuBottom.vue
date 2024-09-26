5afb526eccf2fb26c27e2c98fb6960a5a81494ab
MinhMonster <dominh020195@gmail.com>
2024-09-26 16:58:29 +0700

    fix width header footer

--- components/pages/client/layout/MenuBottom.vue
+++ components/pages/client/layout/MenuBottom.vue
@@ -1,49 +1,51 @@
 <template>
   <client-only>
     <div id="menu-bottom">
-      <ModalSupport />
+      <div class="body-bottom">
+        <ModalSupport />
 
-      <ModalGame />
+        <ModalGame />
 
-      <div class="sub-menu-buttom" @click="nextPath('/')">
-        <div class="footer_icon home">
-          <div class="circle-menu">
-            <div class="icon-wrap">
-              <BaseSvg
-                id="btn-home"
-                title="Trang Chủ"
-                aria-label="Trang Chủ"
-                name="home"
-              />
+        <div class="sub-menu-buttom" @click="nextPath('/')">
+          <div class="footer_icon home">
+            <div class="circle-menu">
+              <div class="icon-wrap">
+                <BaseSvg
+                  id="btn-home"
+                  title="Trang Chủ"
+                  aria-label="Trang Chủ"
+                  name="home"
+                />
+              </div>
             </div>
           </div>
+          <span class="title-menu-buttom">Trang chủ</span>
         </div>
-        <span class="title-menu-buttom">Trang chủ</span>
-      </div>
-      <!-- <ModalSetting /> -->
-      <div class="sub-menu-buttom" @click="nextPath('/topics')">
-        <div class="footer_icon service">
-          <BaseSvg
-            class="icon-menu"
-            id="btn-topics"
-            title="Bài Đăng"
-            aria-label="Bài Đăng"
-            name="book"
-          />
+        <!-- <ModalSetting /> -->
+        <div class="sub-menu-buttom" @click="nextPath('/topics')">
+          <div class="footer_icon service">
+            <BaseSvg
+              class="icon-menu"
+              id="btn-topics"
+              title="Bài Đăng"
+              aria-label="Bài Đăng"
+              name="book"
+            />
+          </div>
+          <span class="title-menu-buttom">Bài Đăng</span>
         </div>
-        <span class="title-menu-buttom">Bài Đăng</span>
-      </div>
-      <div class="sub-menu-buttom" @click="changeTheme()">
-        <div class="footer_icon service">
-          <BaseSvg
-            class="icon-menu"
-            id="btn-change-theme"
-            title="Đổi Màu"
-            aria-label="Đổi Màu"
-            name="theme-light-dark"
-          />
+        <div class="sub-menu-buttom" @click="changeTheme()">
+          <div class="footer_icon service">
+            <BaseSvg
+              class="icon-menu"
+              id="btn-change-theme"
+              title="Đổi Màu"
+              aria-label="Đổi Màu"
+              name="theme-light-dark"
+            />
+          </div>
+          <span class="title-menu-buttom">Đổi Màu</span>
         </div>
-        <span class="title-menu-buttom">Đổi Màu</span>
       </div>
     </div>
   </client-only>
