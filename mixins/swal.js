5647774dd64b85d3008e01eef3c233b88bcd230f
MinhMonster <dominh020195@gmail.com>
2024-11-07 03:18:47 +0700

    refactor show Swal

--- mixins/swal.js
+++ mixins/swal.js
@@ -1,14 +1,25 @@
 
 export default {
   methods: {
-    async showSwal(input) {
+    async showSwal(input = {}) {
+      // Set default class based on route path
+      const defaultClass = this.path.includes('/admin/') ? 'admin' : '';
+
+      // Create a custom class object with default
       const customClass = {
         customClass: {
-          container: this.path.includes('/admin/') ? "admin" : "",
-        }
-      }
-      const setting = Object.assign(input, customClass);
-      this.$swal.fire(setting);
-    },
+          container: defaultClass,
+        },
+      };
+
+      // Merge input options with custom class (override defaults)
+      const setting = Object.assign({}, customClass, input);
+
+      // Call Swal with merged options
+      const result = await this.$swal.fire(setting);
+
+      // Return true/false based on confirmed result
+      return result.isConfirmed;
+    }
   },
 };
