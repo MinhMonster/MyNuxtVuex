75c18511d69484761eecd575c6b98cabf590e8fa
MinhMonster <dominh020195@gmail.com>
2025-07-26 12:01:25 +0700

    change store cookie

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -4,8 +4,8 @@ export default function ({ redirect, req, route, store }) {
 
   if (_.get(req, 'headers.cookie')) {
     const parsedCookies = cookie.parse(req.headers.cookie)
-    if (parsedCookies['MinhMonster']) {
-      const state = JSON.parse(parsedCookies['MinhMonster'])
+    if (parsedCookies['mbn']) {
+      const state = JSON.parse(parsedCookies['mbn'])
       const isLogin = state.admin.auth.token && state.admin.auth.authenticated
       if (!isLogin) {
         return redirect('/admin/login')
--- middleware/un_authentication.js
+++ middleware/un_authentication.js
@@ -2,8 +2,8 @@ import cookie from 'cookie'
 export default function ({ redirect, req, route, store }) {
   if (_.get(req, 'headers.cookie')) {
     const parsedCookies = cookie.parse(req.headers.cookie)
-    if (parsedCookies['MinhMonster']) {
-      const state = JSON.parse(parsedCookies['MinhMonster'])
+    if (parsedCookies['mbn']) {
+      const state = JSON.parse(parsedCookies['mbn'])
       const isLogin = state.admin.auth.token && state.admin.auth.authenticated
       if (isLogin) {
         return redirect('/admin')
